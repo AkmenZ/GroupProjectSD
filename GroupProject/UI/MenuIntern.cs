355f8c46c46ae046a7387dc39c43ccff389f74b0
AkmenZ <akmenz89@gmail.com>
2024-12-21 21:20:44 +0000

    Added missing MenuIntern methods

--- GroupProject/UI/MenuIntern.cs
+++ GroupProject/UI/MenuIntern.cs
@@ -10,11 +10,39 @@ namespace ProjectManagementApp
     {
         public override void DisplayMenu()
         {
-
+            Console.WriteLine("  [1] List Assigned Tasks by Project");
+            Console.WriteLine("  [2] Update Task Status");
+            Console.WriteLine("  [3] Start Task");
+            Console.WriteLine("  [4] Complete Task");
+            Console.WriteLine("  [5] Change Password");
+            Console.WriteLine("  [0] Logout");
         }
         public override void HandleMenuChoice(int choice, IServicesUI servicesUI)
         {
-            
+            switch (choice)
+            {
+                case 1:
+                    servicesUI.ListUserTasks();
+                    break;
+                case 2:
+                    servicesUI.UpdateTaskStatus();
+                    break;
+                case 3:
+                    servicesUI.StartTask();
+                    break;
+                case 4:
+                    servicesUI.CompleteTask();
+                    break;
+                case 5:
+                    servicesUI.ChangePassword();
+                    break;
+                case 0:
+                    servicesUI.Logout();
+                    break;
+                default:
+                    Console.WriteLine("\n  Invalid choice.");
+                    break;
+            }
         }
     }
 }
